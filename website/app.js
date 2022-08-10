bcb89794a7fe68b6c939a753caf6c65c64e41aff
AymanHassanDahab <66025405+AymanHassanDahab@users.noreply.github.com>
2022-08-10 16:10:10 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,83 +1,89 @@
 /* Global Variables */
-const apiKey = 'd87c135aefb93a8d394fd35464d56e10&units=imperial';
-const apiUrl = 'http://localhost:8000/';
+
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = "&appid=d87c135aefb93a8d394fd35464d56e10&units=imperial";
 
 const zipCode = document.querySelector('#zip');
 const feeling = document.querySelector('#feelings');
-const date = document.querySelector('#date');
+const updatedDate = document.querySelector('#date');
 const temp = document.querySelector('#temp');
 const content = document.querySelector('#content');
 
 
-const catchError = function(error) {
-    console.log('Error is ', error);
-} 
+document.getElementById('generate').addEventListener('click', performAction);
 
-// Add function to HTML Element
-document.querySelector('#generate').addEventListener("click", function() {
-    debugger
-    let data = {
-        zipCode: zipCode.value,
-        content: feeling.value,
-        date: new Date()
-    };
-
-    // postData to api to get zipcode data
-    getZipCode(data.zipCode).then(function(zipCodeInfo) {
-        // check if city isn't found and alert it
-        if (zipCodeInfo.cod != 200) {
-            return alert(zipCodeInfo.message)
-        }
-        
-        // Post data to server
-        data.temp = zipCodeInfo.list[0].main.temp;
-        postDataToServer(data);
-    }).catch(catchError);
-});
-
-// get zip code from API
-async function getZipCode(zipCode) {
-    return await(await fetch(`http://api.openweathermap.org/data/2.5/forcast?zip=${zipCode}${apiKey}`)).json()
-}
-
-// for saving, post data to server
-async function postDataToServer(data) {
-    debugger
-    let response = await fetch(`${apiUrl}postData`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        // Body data type must match "Content-Type" header        
-        body: JSON.stringify(data),  
+function performAction(e){
+    const cretaedURL = `${baseURL}${zipCode.value}${apiKey}`;
+    retrieveData(cretaedURL).then(function(data) {
+        filteredData(data).then(function(summaryData) {
+            postData("/addData", summaryData).then(function(data){
+                getBackData("/all", data);
+            });
+        });
     });
+}
+
+// Get the data from openweather website
+const retrieveData = async (url) => {
+    const request = await fetch(url);
     try {
-        if(!response.ok) {
-            alert("Failed Process");
-            return;
+         // Transform into JSON
+        const data = await request.json();
+        return data;
+    }catch(error) {
+        // appropriately handle the error
+        console.log("error", error);
+    }
+}
+
+// Summarize the data needed out of all data receieved.
+const filteredData = async (data) => {
+    if (data.cod != 200){
+        console.log("Error");
+    }else{
+        const importantInfo = {
+            date: new Date().toDateString(),
+            temp: data.main.temp,
+            content: feeling.value
         }
-        response.json().then(function(data) {
-            if (response.ok) {
-                //Update UI right away 
-                updateUI(); 
-            } else {
-                alert('Failed Process');
-            }
-        }).catch(catchError);
-    } catch (error) {
-        catchError(error);
+        return importantInfo;
     }
 }
 
-async function updateUI() {
-    debugger 
-    let response = await fetch(`${apiUrl}all`);
+
+// Async POST
+const postData = async (url = '', data = {})=>{
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
     try {
-        response.json().then(function(){
-            date.innerHTML = `Date: ${data.date}`,
-            temp.innerHTML = `Temperature: ${data.temp}`,
-            content.innerHTML = `Feeling: ${data.content}`
-        }).catch(catchError);
-    } catch(error) {
-        catchError(error);
+        const newData = await response.json();
+        return newData;
+    }catch(error) {
+        // appropriately handle the error
+        console.log("error", error);
     }
-}
+};
 
+// Async GET
+const getBackData = async () =>{
+    const request = await fetch('/all');
+    try {
+    // Transform into JSON
+    const allData = await request.json()
+    console.log(allData)
+    // Write updated data to DOM elements
+    temp.innerHTML = Math.round(allData.temp)+ 'degrees';
+    content.innerHTML = allData.content;
+    updatedDate.innerHTML = new Date().toDateString();
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+   }
